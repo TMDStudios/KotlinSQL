da7a0932c887be479240c40a7d5a23b1ea0198fa
TMDStudios <tmdstudioscontact@gmail.com>
2021-10-19 06:18:07 -0400

    Save and Retrieve

--- app/src/main/java/com/example/kotlinsql/adapters/NoteAdapter.kt
+++ app/src/main/java/com/example/kotlinsql/adapters/NoteAdapter.kt
@@ -1,5 +1,37 @@
 package com.example.kotlinsql.adapters
 
-/**
- * We will add a Recycler View here to display our notes in the next iteration of this app
- * */
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlinsql.MainActivity
+import com.example.kotlinsql.R
+import com.example.kotlinsql.database.NoteModel
+import kotlinx.android.synthetic.main.note_row.view.*
+
+class NoteAdapter(
+    private val activity: MainActivity,
+    private val items: ArrayList<NoteModel>): RecyclerView.Adapter<NoteAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.ItemViewHolder {
+        return ItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.note_row,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: NoteAdapter.ItemViewHolder, position: Int) {
+        val item = items[position]
+
+        holder.itemView.apply {
+            tvNote.text = item.noteText
+        }
+    }
+
+    override fun getItemCount() = items.size
+}
