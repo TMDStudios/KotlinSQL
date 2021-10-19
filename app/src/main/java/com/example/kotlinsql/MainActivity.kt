4e4562d7534b49665ce28e3dee37a1b361f82078
TMDStudios <tmdstudioscontact@gmail.com>
2021-10-19 05:32:50 -0400

    Notes App (Save Only)

--- app/src/main/java/com/example/kotlinsql/MainActivity.kt
+++ app/src/main/java/com/example/kotlinsql/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.example.kotlinsql
 
-import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.kotlinsql.adapters.NoteAdapter
+import android.widget.Toast
 import com.example.kotlinsql.database.DatabaseHandler
 import com.example.kotlinsql.database.NoteModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var db: DatabaseHandler
 
-    private lateinit var rvNotes: RecyclerView
     private lateinit var editText: EditText
     private lateinit var submitBtn: Button
 
@@ -28,51 +23,11 @@ class MainActivity : AppCompatActivity() {
         editText = findViewById(R.id.tvNewNote)
         submitBtn = findViewById(R.id.btSubmit)
         submitBtn.setOnClickListener { postNote() }
-
-        rvNotes = findViewById(R.id.rvNotes)
-        updateRV()
-    }
-
-    private fun updateRV(){
-        rvNotes.adapter = NoteAdapter(this, getItemsList())
-        rvNotes.layoutManager = LinearLayoutManager(this)
-    }
-
-    private fun getItemsList(): ArrayList<NoteModel>{
-        return db.viewNotes()
     }
 
     private fun postNote(){
         db.addNote(NoteModel(0, editText.text.toString()))
         editText.text.clear()
-        updateRV()
-    }
-
-    private fun editNote(noteID: Int, noteText: String){
-        db.updateNote(NoteModel(noteID, noteText))
-        updateRV()
-    }
-
-    fun deleteNote(noteID: Int){
-        db.deleteNote(NoteModel(noteID, ""))
-        updateRV()
-    }
-
-    fun raiseDialog(id: Int){
-        val dialogBuilder = AlertDialog.Builder(this)
-        val updatedNote = EditText(this)
-        updatedNote.hint = "Enter new text"
-        dialogBuilder
-            .setCancelable(false)
-            .setPositiveButton("Save", DialogInterface.OnClickListener {
-                    _, _ -> editNote(id, updatedNote.text.toString())
-            })
-            .setNegativeButton("Cancel", DialogInterface.OnClickListener {
-                    dialog, _ -> dialog.cancel()
-            })
-        val alert = dialogBuilder.create()
-        alert.setTitle("Update Note")
-        alert.setView(updatedNote)
-        alert.show()
+        Toast.makeText(this, "Note Added", Toast.LENGTH_LONG).show()
     }
 }
